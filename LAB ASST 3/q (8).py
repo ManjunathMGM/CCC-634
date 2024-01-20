86dc0eb433da4401d293a88d9c83a6dd4d58292c
Manjunath MGM <manjunathmajety@gmail.com>
2024-01-20 23:21:02 +0530

    Update q (8).py

--- LAB ASST 3/q (8).py
+++ LAB ASST 3/q (8).py
@@ -1,3 +1,6 @@
 number = int((input("Enter a 5 digit even number: ")))
-digits = (number // 10000) % 10 + (number // 1000) % 10 + (number // 100) % 10 + (number // 10) % 10 + (number % 10)
-print("Sum of digits:", digits)
+if len(str(number)) != 5:
+    print("not a 5 digit number")
+else:
+    digits = (number // 10000) % 10 + (number // 1000) % 10 + (number // 100) % 10 + (number // 10) % 10 + (number % 10)
+    print("Sum of digits:", digits)
