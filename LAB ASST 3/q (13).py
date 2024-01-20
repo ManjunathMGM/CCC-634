5b838997d43c0031824223fbc32dfba09b1d488c
Manjunath MGM <manjunathmajety@gmail.com>
2024-01-20 23:09:30 +0530

    Update q (13).py

--- LAB ASST 3/q (13).py
+++ LAB ASST 3/q (13).py
@@ -1,3 +1,4 @@
+
 t1 = float(input("Enter the percentage for Teacher 1: "))
 t2 = float(input("Enter the percentage for Teacher 2: "))
 t3 = float(input("Enter the percentage for Teacher 3: "))
@@ -8,10 +9,10 @@ t7 = float(input("Enter the percentage for Teacher 7: "))
 
 average = (t1 + t2 + t3 + t4 + t5 + t6 + t7) / 7
 
-invitation = bool(input("Letter of Invitation? (true/false): "))
-hod = bool(input("Permission from HOD? (true/false): "))
+invitation = (input("Letter of Invitation? (yes/no): ")).lower()
+hod = (input("Permission from HOD? (yes/no): "))
 
-eligible = average > 80 and (invitation or hod)
+eligible = average > 80 and (invitation == "yes" or hod == "yes")
 
 print(f"\nAverage Percentage: {average}%")
 print("Eligibility for Special Python Training Session - ", eligible)
