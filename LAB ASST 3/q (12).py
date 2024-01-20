722197172e08822f3c9dd1923b6faa819723bfaa
Manjunath MGM <manjunathmajety@gmail.com>
2024-01-20 23:56:30 +0530

    Update q (12).py

--- LAB ASST 3/q (12).py
+++ LAB ASST 3/q (12).py
@@ -1,12 +1,18 @@
-ip1 = bool(input("True or False? "))
-ip2 = bool(input("True or False? "))
+ip1 = input("Enter the 1st boolean value (True/False): ").lower()
+ip2 = input("Enter the 2nd boolean value (True/False): ").lower()
 
-r_and = ip1 and ip2
-r_or = ip1 or ip2
-r_not1 = not ip1
-r_not2 = not ip2
+v1 = ip1 == "true"
+v2 = ip2 == "true"
 
-print(f"Answer for AND: {r_and}")
-print(f"Answer for OR: {r_or}")
-print(f"Answer for NOT (first value): {r_not1}")
-print(f"Answer for NOT (second value): {r_not2}")
+print(v1, v2)
+
+result_and = v1 and v2
+result_or = v1 or v2
+result_not1 = not v1
+result_not2 = not v2
+
+print("Results:")
+print(f"AND - {result_and}")
+print(f"OR - {result_or}")
+print(f"NOT for the first value - {result_not1}")
+print(f"NOT for the second value - {result_not2}")
